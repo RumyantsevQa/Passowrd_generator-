6c00c5f1fc13d3230fedbf4190d0583708e40330
RumyantsevQa <Rumyantsev44@yandex.ru>
2022-10-10 13:26:56 +0300

    Part 5 final

--- generator/views.py
+++ generator/views.py
@@ -11,7 +11,14 @@ def password(request):
 
     characters = list('abcdefghijklmnopqstuvwxyz')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('*ˆ#@$'))
+    if request.GET.get('nambers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length',12))
 
     thepassword = ''
     for x in range(length):
