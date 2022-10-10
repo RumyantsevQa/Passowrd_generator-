642e0dcfad5a3fc2678e674db804851875d5f0b5
RumyantsevQa <Rumyantsev44@yandex.ru>
2022-10-10 16:50:56 +0300

    Finish

--- passowrd_generator/urls.py
+++ passowrd_generator/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.urls import path
 from generator import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('password', views.password, name='password')
+    path('password/', views.password, name='password'),
+    path('about/', views.about, name='about')
 ]
